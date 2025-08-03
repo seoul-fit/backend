e72319e3b87c87ad1b4c2c099b5c1029cf17c2c3
hkjin <wlsgusrb78@naver.com>
2025-08-03 17:59:46 +0900

    :fire: merge

--- src/main/java/com/seoulfit/backend/shared/config/SwaggerConfig.java
+++ src/main/java/com/seoulfit/backend/shared/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.seoulfit.backend.config;
+package com.seoulfit.backend.shared.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
